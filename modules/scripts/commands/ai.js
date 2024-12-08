a28ba9dc852b7223f26bb763a1efc6d8b5d604e3
Lazy-dev-hash <nelzys52@gmail.com>
2024-12-08 09:03:57 +0800

    Update ai.js

--- modules/scripts/commands/ai.js
+++ modules/scripts/commands/ai.js
@@ -1,5 +1,5 @@
-const axios = require("axios");
-const { sendMessage } = require('../handles/sendMessage');
+const fs = require("fs");
+const path = require("path");
 
 module.exports = {
   name: "gpt4o",
